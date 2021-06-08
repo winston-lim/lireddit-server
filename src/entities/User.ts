16201201f142eea116f0bb775048b8ce1edcef39
Low Siew Kam Honey Nicole <winston_lim1@hotmail.com>
2021-06-08 10:51:16 +0800

    Format code

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,25 +1,25 @@
-import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
-import { Field, ObjectType } from "type-graphql";
+import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
+import { Field, ObjectType } from 'type-graphql';
 
 @ObjectType()
 @Entity()
 export class User {
-  @Field()
-  @PrimaryKey()
-  id!: number;
-  
-  @Field( ()=> String )
-  @Property({ type: 'date' })
-  createdAt = new Date()
+	@Field()
+	@PrimaryKey()
+	id!: number;
 
-  @Field( ()=> String )
-  @Property({ type: 'date', onUpdate: ()=> new Date() })
-  updatedAt = new Date();
+	@Field(() => String)
+	@Property({ type: 'date' })
+	createdAt = new Date();
 
-  @Field()
-  @Property({ type: 'text', unique: true })
-  username!: string;
+	@Field(() => String)
+	@Property({ type: 'date', onUpdate: () => new Date() })
+	updatedAt = new Date();
 
-  @Property({ type: 'text'})
-  password!: string;
+	@Field()
+	@Property({ type: 'text', unique: true })
+	username!: string;
+
+	@Property({ type: 'text' })
+	password!: string;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import "reflect-metadata";
+import 'reflect-metadata';
 import { MikroORM } from '@mikro-orm/core';
 import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
@@ -12,56 +12,55 @@ import mikroConfig from './mikro-orm.config';
 import { HelloResolver } from './resolvers/hello';
 import { PostResolver } from './resolvers/post';
 import { UserResolver } from './resolvers/user';
-import { MyContext } from "./types";
+import { MyContext } from './types';
 
 declare module 'express-session' {
-  export interface SessionData {
-    userId: number;
-  }
+	export interface SessionData {
+		userId: number;
+	}
 }
 
 const start = async () => {
-  const orm = await MikroORM.init(mikroConfig);
-  await orm.getMigrator().up();
-  const app = express();
+	const orm = await MikroORM.init(mikroConfig);
+	await orm.getMigrator().up();
+	const app = express();
 
-  const RedisStore = connectRedis(session);
-  const redisClient = redis.createClient();
+	const RedisStore = connectRedis(session);
+	const redisClient = redis.createClient();
 
-  app.use(
-    session({
-      name: 'qid',
-      store: new RedisStore({
-        client: redisClient,
-        disableTouch: true,
-        disableTTL: true,
-      }),
-      cookie: {
-        maxAge: 1000 * 60 * 60 * 24 * 365 * 10, //10 years in milliseconds
-        httpOnly: true, //cannot access cookie from front end
-        secure: __prod__, //cookie only works in https
-        sameSite: 'lax', //csrf
-      },
-      saveUninitialized: false,
-      secret: 'random secret',
-      resave: false,
-    })
-  )
+	app.use(
+		session({
+			name: 'qid',
+			store: new RedisStore({
+				client: redisClient,
+				disableTouch: true,
+				disableTTL: true,
+			}),
+			cookie: {
+				maxAge: 1000 * 60 * 60 * 24 * 365 * 10, //10 years in milliseconds
+				httpOnly: true, //cannot access cookie from front end
+				secure: __prod__, //cookie only works in https
+				sameSite: 'lax', //csrf
+			},
+			saveUninitialized: false,
+			secret: 'random secret',
+			resave: false,
+		})
+	);
 
-  const apolloServer = new ApolloServer({
-    schema: await buildSchema({
-      resolvers: [HelloResolver, PostResolver, UserResolver],
-      validate: false, //to avoid using default validator
-    }),
-    context: ({req, res}): MyContext => ({ em: orm.em, req, res }),
-  });
+	const apolloServer = new ApolloServer({
+		schema: await buildSchema({
+			resolvers: [HelloResolver, PostResolver, UserResolver],
+			validate: false, //to avoid using default validator
+		}),
+		context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
+	});
 
-  apolloServer.applyMiddleware({ app });
+	apolloServer.applyMiddleware({ app });
 
-  app.listen(4000, () => {
-    console.log('server started on localhost:4000');
-  });
-}
+	app.listen(4000, () => {
+		console.log('server started on localhost:4000');
+	});
+};
 
 start();
-
